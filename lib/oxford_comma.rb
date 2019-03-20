2b2da1c1a27241f397792ec38c1bc04da8cca7bb
Mariana Garman <garman.mariana@gmail.com>
2019-03-20 01:53:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.count == 1  
+    array.join 
+  elsif array.count == 2
+      array.join(" and ")
+  else 
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end
 end
+
+# Brad and Mary need to ...
+# Brad, Mary and Kim need...
